05dfbb97adb946badb0cd1772d294a40fcb187d9
noor shamasna <noorshamasna@noors-MacBook-Air.local>
2026-07-08 18:00:47 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '60ec6f6c-0aab-422a-bf19-9f05601ca3d6';
+export const MODAL_CLOSE_SEC = 2.5;
